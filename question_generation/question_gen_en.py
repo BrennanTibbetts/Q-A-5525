0a5fe030cc6803d6f8c80d63124bfcc62d131a0a
Will Blanton <blanton.150@buckeyemail.osu.edu>
2024-03-27 13:56:10 -0400

    Added class for question generation

---
+++ question_generation/question_gen_en.py
@@ -0,0 +1,25 @@
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSeq2SeqLM
+)
+
+
+class QuestionGenerator:
+
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("voidful/context-only-question-generator")
+        self.model = AutoModelForSeq2SeqLM.from_pretrained("voidful/context-only-question-generator")
+
+    def generate_questions(self, context: str, num_questions: int = 4, temperature: float = 1.0):
+        """
+        Generate questions from a given context using the model
+        """
+
+        input_ids = self.tokenizer(context, return_tensors="pt").input_ids
+        outputs = self.model.generate(input_ids,
+                                      max_new_tokens=50,
+                                      num_return_sequences=num_questions,
+                                      temperature=temperature,
+                                      do_sample=True)
+        questions = list({self.tokenizer.decode(output, skip_special_tokens=True) for output in outputs})
+        return questions
