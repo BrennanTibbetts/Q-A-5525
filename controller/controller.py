3af26a208afaf3e0d97d2f2f8de4d8f1353a10c2
Will-Blanton <blanton.150@osu.edu>
2024-04-16 22:43:13 -0400

    Refactored the controller to decouple from specific models to avoid relative imports.
    Moved controller evaluation to its own notebook.

--- controller/controller.py
+++ controller/controller.py
@@ -1,59 +1,21 @@
-import os
-import pandas as pd
-import sys
-import torch
-import nltk
-
-from nltk.translate.bleu_score import sentence_bleu
-from nltk.tokenize import word_tokenize
-
-nltk.download('punkt')
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
-from answer_extraction import NER_Extractor
-from question_generation.question_gen_en import QuestionGenerator
-from translation import Translator
-from distraction_generation import DistractionFinder
-
-
-ENGLISH_JSON = "data/xquad.en.json"
-SPANISH_JSON = "data/xquad.es.json"
-
-def load_qa_data(json_file):
-    
-    articles = pd.read_json(json_file)
-
-    # turn the json into a list of dictionaries
-    articles = [a for a in articles["data"]]
-
-    return articles
-
-
-def bleu_comparison(original: str, translated: str):
-    """
-    Compare the generated questions with the dataset questions using BLEU score
-    """
-
-    original_tokens = word_tokenize(original.lower())
-
-    translated_tokens = word_tokenize(translated.lower())
-
-    bleu_score = sentence_bleu([original_tokens], translated_tokens)
-
-    bleu_score = bleu_score if bleu_score >= .0001 else 0
-
-    return bleu_score
-
-
 class Controller:
-    def __init__(self):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    def __init__(self,
+                 translator=None,
+                 extractor=None,
+                 question_generator=None,
+                 distraction_finder=None):
+        """
+        Initialize the controller with the necessary components
+        :param translator: Translator: the translator component
+        :param extractor: NER_Extractor: the answer extraction component
+        :param question_generator: QuestionGenerator: the question generation component
+        :param distraction_finder: DistractionFinder: the distraction generation component
+        """
 
-        self.translator = Translator(device)
-        self.extractor = NER_Extractor()
-        self.question_generator = QuestionGenerator()
-        self.distraction_finder = DistractionFinder()
+        self.translator = translator
+        self.extractor = extractor 
+        self.question_generator = question_generator
+        self.distraction_finder = distraction_finder
 
     # methods to simplify model calls
     def translate(self, text:str):
@@ -89,65 +51,3 @@ class Controller:
             qa_pairs.append((question, a, distractions))
         
         return context, qa_pairs
-        
-
-
-def score_qa_pair(controller, english: dict, spanish: dict, display: bool = False):
-    """
-    Iterate over the articles and paragraphs in the English and Spanish data to translate,
-    the Spansih data is translated to English and then generate questions and answers in English.
-    Evaluate 
-    """
-
-    # iterate through the articles and paragraphs
-    for i, article in enumerate(english[0:1]):
-        for j, paragraph in enumerate(article["paragraphs"]):
-            if display and j >= 1:
-                break
-
-            # get the text to translate
-            spanish_context = spanish[i]["paragraphs"][j]["context"]
-
-            translated_context, qa_pairs = controller.gen_qa_pairs(spanish_context)
-
-            # get the correct translation
-            target_context = paragraph["context"]
-
-            if display:
-                print("--------------------------------------------------\n")
-                print(f"Spanish Context: {spanish_context}\n")
-                print(f"English Context: {target_context}\n")
-                print(f"Translated Context: {translated_context}\n")
-                print("--------------------------------------------------\n")
-
-            bleu_score = bleu_comparison(target_context, translated_context)
-            print(f"BLEU Score: {bleu_score}\n")
-
-            target_qa = []
-            for qas in paragraph["qas"]:
-                target_question = qas["question"]
-                target_answer = [a["text"] for a in qas["answers"]]
-                
-                target_qa.append((target_question, target_answer))
-
-            for gen_q, extr_a, extr_dist in qa_pairs:
-
-                if display:
-                    print("--------------------------------------------------\n")
-                    print(f"Generated-Q: {gen_q}\n")
-                    print(f"Extracted-A: {extr_a}\n")
-                    print(f"Distractions: {extr_dist}\n")
-                    # print(f"Target-QA: {target_qa}\n")
-                    print("--------------------------------------------------\n")
-
-            
-
-
-if __name__ == "__main__":
-    controller = Controller()
-
-    english_qa = load_qa_data(ENGLISH_JSON)
-    spanish_qa = load_qa_data(SPANISH_JSON)
-
-    # score single pair for display
-    score_qa_pair(controller, english_qa, spanish_qa, display=True)
